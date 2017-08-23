b2b9bc82803df530c2ef734614c620d4ea8547b7
Tom Meschter <tom.meschter@microsoft.com>
2017-08-23 13:41:16 -0700

    Attemp to get Authentication/Authorization working
    Changes based on code from https://github.com/Azure-Samples/active-directory-dotnet-webapp-openidconnect-aspnetcore/tree/aspnet_core_2_0.

--- StinkyPig/Startup.cs
+++ StinkyPig/Startup.cs
@@ -7,6 +7,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authentication;
 
 namespace StinkyPig
 {
@@ -29,6 +32,26 @@ namespace StinkyPig
         {
             // Add framework services.
             services.AddMvc();
+
+            // Add authentication
+            services.AddAuthentication(sharedOptions =>
+            {
+                sharedOptions.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+                sharedOptions.DefaultChallengeScheme = OpenIdConnectDefaults.AuthenticationScheme;
+            })
+            .AddCookie()
+            .AddOpenIdConnect(option =>
+            {
+                option.ClientId = Configuration["AzureAD:ClientId"];
+                option.Authority = String.Format(Configuration["AzureAd:AadInstance"], Configuration["AzureAd:Tenant"]);
+                option.SignedOutRedirectUri = Configuration["AzureAd:PostLogoutRedirectUri"];
+                option.Events = new OpenIdConnectEvents
+                {
+
+                    OnRemoteFailure = OnAuthenticationFailed,
+
+                };
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,6 +59,8 @@ namespace StinkyPig
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
+
+            app.UseAuthentication();
 
             if (env.IsDevelopment())
             {
@@ -55,6 +80,15 @@ namespace StinkyPig
                     name: "default",
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
+        }
+
+        // Handle sign-in errors differently than generic errors.
+
+        private Task OnAuthenticationFailed(RemoteFailureContext context)
+        {
+            context.HandleResponse();
+            context.Response.Redirect("/Home/Error?message=" + context.Failure.Message);
+            return Task.FromResult(0);
         }
     }
 }
